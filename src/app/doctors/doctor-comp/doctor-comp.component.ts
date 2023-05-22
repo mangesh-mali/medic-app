42610c6973215fa7b5a6828da56049796dd51d75
mangeshmali01 <mangesh.v.mali@gmail.com>
2023-05-22 16:40:04 +0530

    doctors module completion

--- src/app/doctors/doctor-comp/doctor-comp.component.ts
+++ src/app/doctors/doctor-comp/doctor-comp.component.ts
@@ -8,6 +8,9 @@ import { Ser1Service } from 'src/app/Services/ser1.service';
 })
 export class DoctorCompComponent {
   data: any;
+  displayStyle = "none";
+  gen: any = ['Male', 'Female'];
+  genSelect: any;
 
   constructor(private ser: Ser1Service) {
     this.getData();
@@ -21,6 +24,54 @@ export class DoctorCompComponent {
       }),
       ((err: any) => { console.log(err); })
     )
+  }
+
+  openPopup() {
+    this.clearForm();
+    this.displayStyle = "block";
+  }
+
+  clearForm() {
+    //clear form
+    var allInputs = document.querySelectorAll('input');
+    allInputs.forEach(singleInput => singleInput.value = '');
+  }
+
+  closePopup() {
+    this.displayStyle = "none";
+  }
+
+  addDoc(f: any) {
+    console.log(f.value);
+    var obj = f.value;
+    if (obj.gen == "Male") {
+      obj.img = "http://medic-app.type-code.pro/assets/content/doctor-400-2.jpg";
+    }
+    if (obj.gen == "Female") {
+      obj.img = "http://medic-app.type-code.pro/assets/content/doctor-400-4.jpg";
+    }
+    delete obj.gen;
+    obj.social = [
+      { "icon": "assets/1-removebg-preview.png" },
+      { "icon": "assets/2-removebg-preview.png" },
+      { "icon": "assets/3-removebg-preview.png" }
+    ]
+    if (!obj.name.includes("Dr")) {
+      obj.name = "Dr. " + obj.name;
+    }
+
+    console.log(obj);
+
+    this.ser.addDoc(obj).subscribe(
+      ((res: any) => {
+        console.log(res);
+        this.getData();
+      }),
+      ((err: any) => { console.log(err); })
+    )
+
+    this.clearForm();
+    this.closePopup();
   }
 
 }
--- src/app/doctors/doctors.module.ts
+++ src/app/doctors/doctors.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoctorCompComponent } from './doctor-comp/doctor-comp.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'doctors', component: DoctorCompComponent }
@@ -13,7 +14,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule
   ]
 })
 export class DoctorsModule { }
