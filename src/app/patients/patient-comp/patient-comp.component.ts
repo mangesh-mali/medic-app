f82c1f78a2185d86ff26db44b718658064a9c2c2
mangeshmali01 <mangesh.v.mali@gmail.com>
2023-05-22 19:57:43 +0530

    patients module completion

--- src/app/patients/patient-comp/patient-comp.component.ts
+++ src/app/patients/patient-comp/patient-comp.component.ts
@@ -8,6 +8,17 @@ import { Ser1Service } from 'src/app/Services/ser1.service';
 })
 export class PatientCompComponent {
   data: any;
+  displayStyle = "none";
+  status: any = ["Approved", "Pending"];
+  nm: any;
+  ag: any;
+  adr: any;
+  num: any;
+  selStatus: any;
+  idd: any;
+  image: any;
+  gen: any;
+  lV: any;
 
   constructor(private ser: Ser1Service) {
     this.getAPI();
@@ -25,12 +36,60 @@ export class PatientCompComponent {
     )
   }
 
-  edit(a: any) {
-
+  openPopup() {
+    // this.clearForm();
+    this.displayStyle = "block";
   }
 
-  del(a: any) {
+  clearForm() {
+    //clear form
+    var allInputs = document.querySelectorAll('input');
+    allInputs.forEach(singleInput => singleInput.value = '');
+  }
 
+  closePopup() {
+    this.clearForm();
+    this.displayStyle = "none";
+  }
+
+  update(f: any) {
+    // console.log(f.value);
+    var temp = f.value;
+    temp.gender = this.gen;
+    temp.img = this.image;
+    temp.id = this.idd;
+    console.log(temp);
+
+    this.ser.updatePat(temp).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.getAPI();
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    )
+
+    this.closePopup();
+  }
+
+  edit(a: any) {
+    this.openPopup();
+    this.nm = a.name;
+    this.ag = a.age;
+    this.adr = a.address;
+    this.num = a.number;
+    this.idd = a.id;
+    this.selStatus = a.status;
+    this.lV = a.lastVisit;
+    this.gen = a.gender;
+    this.image = a.img;
+  }
+
+  del(a: any, i: any) {
+    console.log(a);
+    console.log(i);
+    this.data.splice(i, 1);
   }
 
 }
--- src/app/patients/patients.module.ts
+++ src/app/patients/patients.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientCompComponent } from './patient-comp/patient-comp.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: 'patients', component: PatientCompComponent }
@@ -13,7 +14,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule
   ]
 })
 export class PatientsModule { }
